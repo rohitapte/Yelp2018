db96f9b5c9384785f57bbaa97bea44f3c8cc2f11
Rohit Apte <rohit@rohitapte.com>
2018-04-02 16:37:45 +0800

    modules first draft

---
+++ modules.py
@@ -0,0 +1,139 @@
+import tensorflow as tf
+from tensorflow.python.ops.rnn_cell import DropoutWrapper
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import rnn_cell
+
+class BiRNNEncoder(object):
+    def __init__(self,hidden_size,keep_prob):
+        self.hidden_size=hidden_size
+        self.keep_prob=keep_prob
+        self.rnn_cell_fw=rnn_cell.LSTMCell(self.hidden_size)
+        self.rnn_cell_fw=DropoutWrapper(self.rnn_cell_fw,input_keep_prob=self.keep_prob)
+        self.rnn_cell_bw=rnn_cell.LSTMCell(self.hidden_size)
+        self.rnn_cell_bw=DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
+
+    def build_graph(self,inputs,masks):
+        with vs.variable_scope("BiRNNEncoder"):
+            input_lens=tf.reduce_sum(masks,reduction_indices=1)
+            (fw_out,bw_out),_=tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw,self.rnn_cell_bw,inputs,input_lens,dtype=tf.float32)
+            out=tf.concat([fw_out,bw_out],2)
+            out=tf.nn.dropout(out,self.keep_prob)
+            return out
+
+class RNNModelEncoder(object):
+    """
+    General-purpose module to encode a sequence using a RNN.
+    It feeds the input through a RNN and returns all the hidden states.
+
+    Note: In lecture 8, we talked about how you might use a RNN as an "encoder"
+    to get a single, fixed size vector representation of a sequence
+    (e.g. by taking element-wise max of hidden states).
+    Here, we're using the RNN as an "encoder" but we're not taking max;
+    we're just returning all the hidden states. The terminology "encoder"
+    still applies because we're getting a different "encoding" of each
+    position in the sequence, and we'll use the encodings downstream in the model.
+
+    This code uses a bidirectional GRU, but you could experiment with other types of RNN.
+    """
+
+    def __init__(self, hidden_size, keep_prob,model_name="RNNModelEncoder"):
+        """
+        Inputs:
+          hidden_size: int. Hidden size of the RNN
+          keep_prob: Tensor containing a single scalar that is the keep probability (for dropout)
+        """
+        self.hidden_size = hidden_size
+        self.keep_prob = keep_prob
+        self.rnn_cell_fw = rnn_cell.LSTMCell(self.hidden_size)
+        self.rnn_cell_fw = DropoutWrapper(self.rnn_cell_fw, input_keep_prob=self.keep_prob)
+        self.rnn_cell_bw = rnn_cell.LSTMCell(self.hidden_size)
+        self.rnn_cell_bw = DropoutWrapper(self.rnn_cell_bw, input_keep_prob=self.keep_prob)
+        self.model_name=model_name
+
+    def build_graph(self, inputs, masks):
+        """
+        Inputs:
+          inputs: Tensor shape (batch_size, seq_len, input_size)
+          masks: Tensor shape (batch_size, seq_len).
+            Has 1s where there is real input, 0s where there's padding.
+            This is used to make sure tf.nn.bidirectional_dynamic_rnn doesn't iterate through masked steps.
+
+        Returns:
+          out: Tensor shape (batch_size, seq_len, hidden_size*2).
+            This is all hidden states (fw and bw hidden states are concatenated).
+        """
+        with vs.variable_scope(self.model_name):
+            input_lens = tf.reduce_sum(masks, reduction_indices=1) # shape (batch_size)
+
+            # Note: fw_out and bw_out are the hidden states for every timestep.
+            # Each is shape (batch_size, seq_len, hidden_size).
+            (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw, self.rnn_cell_bw, inputs, input_lens, dtype=tf.float32)
+
+            # Concatenate the forward and backward hidden states
+            out = tf.concat([fw_out, bw_out], 2)
+
+            # Apply dropout
+            out = tf.nn.dropout(out, self.keep_prob)
+
+            return out
+
+class SimpleSoftmaxLayer(object):
+    """
+    Module to take set of hidden states, (e.g. one for each context location),
+    and return probability distribution over those states.
+    """
+
+    def __init__(self):
+        pass
+
+    def build_graph(self, inputs, masks):
+        """
+        Applies one linear downprojection layer, then softmax.
+
+        Inputs:
+          inputs: Tensor shape (batch_size, seq_len, hidden_size)
+          masks: Tensor shape (batch_size, seq_len)
+            Has 1s where there is real input, 0s where there's padding.
+
+        Outputs:
+          logits: Tensor shape (batch_size, seq_len)
+            logits is the result of the downprojection layer, but it has -1e30
+            (i.e. very large negative number) in the padded locations
+          prob_dist: Tensor shape (batch_size, seq_len)
+            The result of taking softmax over logits.
+            This should have 0 in the padded locations, and the rest should sum to 1.
+        """
+        with vs.variable_scope("SimpleSoftmaxLayer"):
+
+            # Linear downprojection layer
+            logits = tf.contrib.layers.fully_connected(inputs, num_outputs=1, activation_fn=None) # shape (batch_size, seq_len, 1)
+            logits = tf.squeeze(logits, axis=[2]) # shape (batch_size, seq_len)
+
+            # Take softmax over sequence
+            masked_logits, prob_dist = masked_softmax(logits, masks, 1)
+
+            return masked_logits, prob_dist
+
+def masked_softmax(logits, mask, dim):
+    """
+    Takes masked softmax over given dimension of logits.
+
+    Inputs:
+      logits: Numpy array. We want to take softmax over dimension dim.
+      mask: Numpy array of same shape as logits.
+        Has 1s where there's real data in logits, 0 where there's padding
+      dim: int. dimension over which to take softmax
+
+    Returns:
+      masked_logits: Numpy array same shape as logits.
+        This is the same as logits, but with 1e30 subtracted
+        (i.e. very large negative number) in the padding locations.
+      prob_dist: Numpy array same shape as logits.
+        The result of taking softmax over masked_logits in given dimension.
+        Should be 0 in padding locations.
+        Should sum to 1 over given dimension.
+    """
+    exp_mask = (1 - tf.cast(mask, 'float')) * (-1e30) # -large where there's padding, 0 elsewhere
+    masked_logits = tf.add(logits, exp_mask) # where there's padding, set logits to -large
+    prob_dist = tf.nn.softmax(masked_logits, dim)
+    return masked_logits, prob_dist
